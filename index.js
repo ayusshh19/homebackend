c15dc8eca37950efaf9bb2271eb287be8b64ae67
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2023-11-10 23:28:53 +0530

    data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/budget")
+mongoose.connect("mongodb+srv://cmb49ayushshukla:j7hnqOhhkjvt0UFt@cluster0.sbbjamw.mongodb.net/?retryWrites=true&w=majority")
 
 app.get('/', (req, res) => {
     Spentmodel.find({})
